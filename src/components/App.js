edaa516c323b7724753a80efc08cd0cdaa151a40
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2020-05-14 10:16:13 +0800

    feat() request images from unsplash onSearchSubmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import unsplash from "../api/unsplash";
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    )
-};
+class App extends React.Component {
+
+    state = {images: []};
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: {query: term},
+        });
+        this.setState({images: response.data.results});
+        console.log(this.state.images);
+    };
+
+    render() {
+        return (
+            <div>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        )
+    };
+}
 
 export default App;
